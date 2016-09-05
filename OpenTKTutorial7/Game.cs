548940e8aee686616ab94cbff8515756c55c1b88
Blobby19 <luc.viala19@gmail.com>
2016-09-05 17:41:20 +0200

    - Tutorial7 Added to solution

---
+++ OpenTKTutorial7/Game.cs
@@ -0,0 +1,261 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenTKTutorial7
+{
+    class Game : GameWindow
+    {
+
+        public Game()
+            :base(512, 512, new OpenTK.Graphics.GraphicsMode(32, 24, 0, 4))
+        {
+
+        }
+
+        Vector3[] vertData;
+        Vector3[] colData;
+        Vector2[] texCoordData;
+        int[] indiceData;
+        int ibo_elements;
+        Camera cam = new Camera();
+        Vector2 lastMousePos = new Vector2();
+
+        List<Volume> objects = new List<Volume>();
+        Dictionary<string, int> textures = new Dictionary<string, int>();
+        Dictionary<string, ShaderProgram> shaders = new Dictionary<string, ShaderProgram>();
+        string activeShader = "default";
+
+        float time = 0.0f;
+
+        void initProgram()
+        {
+            lastMousePos = new Vector2(Mouse.X, Mouse.Y);
+
+            GL.GenBuffers(1, out ibo_elements);
+            shaders.Add("default", new ShaderProgram("vs.glsl", "fs.glsl", true));
+            shaders.Add("textured", new ShaderProgram("vs_tex.glsl", "fs_tex.glsl", true));
+
+            activeShader = "textured";
+
+            textures.Add("opentksquare.png", loadImage("opentksquare.png"));
+            textures.Add("opentksquare2.png", loadImage("opentksquare2.png"));
+
+            TexturedCube tc = new TexturedCube();
+            tc.TextureID = textures["opentksquare.png"];
+            objects.Add(tc);
+
+            TexturedCube tc2 = new TexturedCube();
+            tc2.TextureID = textures["opentksquare2.png"];
+            objects.Add(tc2);
+
+            cam.Position += new Vector3(0f, 0f, 3f);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            initProgram();
+
+            Title = "OpenTKTutorial7";
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.PointSize(5f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+
+            GL.Viewport(0, 0, Width, Height);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.Enable(EnableCap.DepthTest);
+
+            shaders[activeShader].EnableVertexAttribArrays();
+
+            int indiceAt = 0;
+
+            foreach(Volume v in objects)
+            {
+                GL.BindTexture(TextureTarget.Texture2D, v.TextureID);
+                GL.UniformMatrix4(shaders[activeShader].GetUniform("modelview"), false, ref v.ModelViewProjectionMatrix);
+
+                if(shaders[activeShader].GetAttribute("maintexture") != -1)
+                {
+                    GL.Uniform1(shaders[activeShader].GetAttribute("maintexture"), v.TextureID);
+                }
+
+                GL.DrawElements(BeginMode.Triangles, v.IndiceCount, DrawElementsType.UnsignedInt, indiceAt * sizeof(uint));
+
+                indiceAt += v.IndiceCount;
+            }
+
+            shaders[activeShader].DisableVertexAttribArrays();
+
+            GL.Flush();
+            SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            List<Vector3> verts = new List<Vector3>();
+            List<int> inds = new List<int>();
+            List<Vector3> colors = new List<Vector3>();
+            List<Vector2> texcoords = new List<Vector2>();
+
+            int vertCount = 0;
+
+            foreach(Volume v in objects)
+            {
+                verts.AddRange(v.GetVerts().ToArray());
+                inds.AddRange(v.GetIndices().ToArray());
+                colors.AddRange(v.GetColorData().ToArray());
+                texcoords.AddRange(v.GetTextureCoords().ToArray());
+                vertCount += v.VertCount;
+            }
+
+            vertData = verts.ToArray();
+            indiceData = inds.ToArray();
+            colData = colors.ToArray();
+            texCoordData = texcoords.ToArray();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, shaders[activeShader].GetBuffer("vPosition"));
+
+            GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(vertData.Length * Vector3.SizeInBytes), vertData, BufferUsageHint.StaticDraw);
+            GL.VertexAttribPointer(shaders[activeShader].GetAttribute("vPosition"), 3, VertexAttribPointerType.Float, false, 0, 0);
+
+            if(shaders[activeShader].GetAttribute("vColor") != -1)
+            {
+                GL.BindBuffer(BufferTarget.ArrayBuffer, shaders[activeShader].GetBuffer("vColor"));
+                GL.BufferData<Vector3>(BufferTarget.ArrayBuffer, (IntPtr)(colData.Length * Vector3.SizeInBytes), colData, BufferUsageHint.StaticDraw);
+                GL.VertexAttribPointer(shaders[activeShader].GetAttribute("vColor"), 3, VertexAttribPointerType.Float, true, 0, 0);
+            }
+
+            if(shaders[activeShader].GetAttribute("texcoord") != -1)
+            {
+                GL.BindBuffer(BufferTarget.ArrayBuffer, shaders[activeShader].GetBuffer("texcoord"));
+                GL.BufferData(BufferTarget.ArrayBuffer, (IntPtr)(texCoordData.Length * Vector2.SizeInBytes), texCoordData, BufferUsageHint.StaticDraw);
+                GL.VertexAttribPointer(shaders[activeShader].GetAttribute("texcoord"), 2, VertexAttribPointerType.Float, true, 0, 0);
+            }
+
+            time += (float)e.Time;
+
+            objects[0].Position = new Vector3(0.3f, -0.5f + (float)Math.Sin(time), -3.0f);
+            objects[0].Rotation = new Vector3(0.55f * time, 0.25f * time, 0);
+            objects[0].Scale = new Vector3(0.5f, 0.5f, 0.5f);
+
+            objects[1].Position = new Vector3(-1f, 0.5f + (float)Math.Cos(time), -2.0f);
+            objects[1].Rotation = new Vector3(-0.25f * time, -0.35f * time, 0);
+            objects[1].Scale = new Vector3(0.7f, 0.7f, 0.7f);
+
+            foreach(Volume v in objects)
+            {
+                v.CalculateModelMatrix();
+                v.ViewProjectionMatrix = cam.GetViewMatrix() * Matrix4.CreatePerspectiveFieldOfView(1.3f, ClientSize.Width / (float)ClientSize.Height, 1.0f, 40.0f);
+                v.ModelViewProjectionMatrix = v.ModelMatrix * v.ViewProjectionMatrix;
+            }
+
+            GL.UseProgram(shaders[activeShader].ProgramID);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ibo_elements);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, (IntPtr)(indiceData.Length * sizeof(int)), indiceData, BufferUsageHint.StaticDraw);
+
+
+            if (Focused)
+            {
+                Vector2 delta = lastMousePos - new Vector2(OpenTK.Input.Mouse.GetState().X, OpenTK.Input.Mouse.GetState().Y);
+                lastMousePos += delta;
+
+                cam.AddRotation(delta.X, delta.Y);
+                ResetCursor();
+            }
+        }
+
+        protected override void OnKeyPress(KeyPressEventArgs e)
+        {
+            base.OnKeyPress(e);
+            if(e.KeyChar == 27)
+            {
+                Exit();
+            }
+
+            switch (e.KeyChar)
+            {
+                case 'z':
+                    cam.Move(0f, 0.1f, 0f);
+                    break;
+                case 'q':
+                    cam.Move(-0.1f, 0f, 0f);
+                    break;
+                case 's':
+                    cam.Move(0f, -0.1f, 0f);
+                    break;
+                case 'd':
+                    cam.Move(0.1f, 0f, 0f);
+                    break;
+                case 'a':
+                    cam.Move(0f, 0f, 0.1f);
+                    break;
+                case 'e':
+                    cam.Move(0f, 0f, -0.1f);
+                    break;
+            }
+        }
+
+        void ResetCursor()
+        {
+            OpenTK.Input.Mouse.SetPosition(Bounds.Left + Bounds.Width / 2, Bounds.Top + Bounds.Height / 2);
+            lastMousePos = new Vector2(OpenTK.Input.Mouse.GetState().X, OpenTK.Input.Mouse.GetState().Y);
+        }
+
+        protected override void OnFocusedChanged(EventArgs e)
+        {
+            base.OnFocusedChanged(e);
+            if (Focused)
+            {
+                ResetCursor();
+            }
+        }
+
+        int loadImage(Bitmap image)
+        {
+            int texID = GL.GenTexture();
+
+            GL.BindTexture(TextureTarget.Texture2D, texID);
+            BitmapData data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0,
+                OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+
+            image.UnlockBits(data);
+
+            GL.GenerateMipmap(GenerateMipmapTarget.Texture2D);
+            return texID;
+        }
+
+        int loadImage(string filename)
+        {
+            try
+            {
+                Bitmap file = new Bitmap(filename);
+                return loadImage(file);
+            }
+            catch(FileNotFoundException ex)
+            {
+                return -1;
+            }
+        }
+    }
+}
